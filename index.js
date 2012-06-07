65852a1bccfacea938c114555ef7af7a6ba5301c
Ian Hansen <ian@supershabam.com>
2012-06-06 23:08:23 -0600

    implements new interface and keeps double-linked list of nodes for eviction

--- index.js
+++ index.js
@@ -3,140 +3,119 @@
  * Copyright(c) 2012 Ian Hansen <ian@supershabam.com>
  * MIT Licensed
  */
-
 "use strict";
 
-var _ = require('underscore')
-  , util = require('util')
+var util = require('util')
   , events = require('events')
-  , NULL = {}
-  , EVICT_REASONS;
-  
-EVICT_REASONS = {
-  EXPIRED: 'expired',
-  REPLACED: 'replaced'
-};
+  , NULL
+  ;
 
 /**
- * Initialize default values
+ * options:
+ * + populate - function to run when getting a value that doesn't exit in the cache
+ * + expires - time in miliseconds that a key can stay in the cache (null for no expiration)
+ * + capacity - maximum number of items in the cache, older items will be evicted to make space (null for no max)
  */
-function Goldfish(timeout, loader, cleanupInterval) {
-  var self = this;
-  events.EventEmitter.call(this);
-  
-  this._timeout = timeout || 1000;
-  this._cleanupInterval = cleanupInterval || 1000;
-  this._loader = loader || this._defaultLoader;
+function Goldfish(options) {
+  options = options || {};
+  this._populate = options.populate || null;
+  this._expires = options.expires || null;
+  this._capacity = options.capacity || null;
   this._cache = {};
-  
-  (function cleanup() {
-    self._cleanup.call(self);
-    self._cleanupTimeout = setTimeout(cleanup, self._cleanupInterval);
-  })();
+  this._newest = NULL;
+  this._oldest = NULL;
+  this._size = 0;
 }
 util.inherits(Goldfish, events.EventEmitter);
 
 /**
- * Get a value from the cache. If not found locally, use loader
+ * key - hashable value to fetch from cache or run populate function with
+ * options:
+ * + populate - override the populate function provided
+ * + refresh - boolean whether or not to touch the key value so that it doesn't expire or get evicted for space (default: true)
  */
-Goldfish.prototype.get = function(key, options) {
-  var cb
-    , loader    
-    , self = this
-    , value;
-  
-  // process options as cb or object
-  if (_.isFunction(options)) {
-    options = {
-      cb: options
-    };
-  } else {
-    options = options || {};
-  }
-  
-  // prepare values
-  cb = options.cb || function() {};
-  loader = options.loader || this._loader;
-  value = this._getValueFromCache(key);
-    
-  // return value from cache
-  if(value !== NULL) {
-    return cb(null, value);
-  }
-  
-  // get the value using loader
-  return loader(key, cb);
-};
+Goldfish.prototype.get = function(key, cb, options) {
+  options = options || {};
 
-/**
- * Evict a key from the cache
- */
-Goldfish.prototype.evict = function(key, reason) {
-  var value = this._getValueFromCache(key);
-  if (value !== NULL) {
-    this._evict(key, value, reason);
-  }
-};
+  var populate = options.populate || this._populate
+    , refresh = options.refresh ? true : false
+    ;
 
-/**
- * Set a key into the cache with a timeout
- */
-Goldfish.prototype.set = function(key, value, timeout) {
-  timeout = timeout || this._timeout;
-  
-  this.evict(key, EVICT_REASONS.REPLACED);
-  
-  this._cache[key] = {
-    value: value,
-    timeout: new Date().getTime() + timeout
-  };
-};
+  // try evicting locally first if expired
 
-/**
- * Default loader that merely complains that it couldn't load anything for you
- */
-Goldfish.prototype._defaultLoader = function(key, cb) {
-  return cb('key not found');
-};
-
-/**
- * Private helper to get a value from the cache, or expiring the value if too old
- */
-Goldfish.prototype._getValueFromCache = function(key) {
-  var now = new Date().getTime();
-    
   if (this._cache.hasOwnProperty(key)) {
-    if (this._cache[key].timeout < now) {
-      this._evict(key, this._cache[key].value, EVICT_REASONS.EXPIRED);
-      return NULL;
-    }
-    return this._cache[key].value;
+    if (refresh) this._refresh(key);
+    process.nextTick(cb.bind({}, null, this._cache[key].value));
+  } else {
+    this._fetch(populate, key, cb);
   }
-  return NULL;
-};
-
-/**
- * Private eviction removes key from cache and emits
- */
-Goldfish.prototype._evict = function(key, value, reason) {
-  this.emit('evict', key, value, reason);
-  this.emit('evict:' + key, value, reason);
-  delete this._cache[key];
 }
 
 /**
- * Evict old cache entries
+ * force the eviction of a key
+ * key - hashable value to evict from local cache
  */
-Goldfish.prototype._cleanup = function() {
-  var self = this
-    , now = new Date().getTime();
-  
-  this.emit('cleanup');  
-  Object.keys(this._cache).forEach(function(key) {
-    if (self._cache[key].timeout < now) {
-      self.evict(key, EVICT_REASONS.EXPIRED);
-    }
+Goldfish.prototype.evict = function(key) {}
+
+// events fired: evict, evict:expired, evict:manual, evict:capacity
+
+Goldfish.prototype._fetch = function(populate, key, cb) {
+  var self = this;
+
+  populate(key, function(err, value) {
+    if (err) return cb(err);
+
+    self._insert(key, value);
+    cb(null, value);
   });
 };
 
-module.exports.Goldfish = Goldfish;
+Goldfish.prototype._insert = function(key, value) {
+  var entry = {
+    value: value,
+    refreshed: new Date(),
+    newer: NULL,
+    older: this._newest
+  };
+
+  if (this._newest !== NULL) {
+    this._newest.newer = entry;
+  }
+
+  this._newest = entry;
+
+  if (this._oldest === NULL) {
+    this._oldest = entry;
+  }
+
+  this._cache[key] = entry;
+}
+
+Goldfish.prototype._refresh = function(key) {
+  var entry = this._cache[key];
+
+  if (entry.older !== NULL) {
+    entry.older.newer = entry.newer;
+  }
+  if (entry.newer !== NULL) {
+    entry.newer.older = entry.older;
+  }
+  if (this._oldest === entry && entry.newer) {
+    this._oldest = entry.newer;
+  }
+
+  entry.refreshed = new Date();
+  entry.newer = NULL;
+  if (this._newest !== entry) {
+    entry.older = this._newest;
+    this._newest.newer = entry;
+    this._newest = entry;
+  }
+}
+
+var g = new Goldfish();
+g._insert('test', '1');
+g._insert('another', '2');
+g._insert('asldfk', '3');
+g._refresh('asldfk');
+console.log(g);
